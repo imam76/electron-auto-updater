f3808a30444e7bd624511f6ae66772d6e3b5104c
imam76 <asepimam335@gmail.com>
2024-03-07 16:02:24 +0700

    ganti pake boiler plate

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,33 +8,7 @@ function Hello() {
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      INI APP UDAH TER BARU 0.0.14
     </div>
   );
 }
