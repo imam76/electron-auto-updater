4c1e531417ee789e81c9c8dbb7e01bc3dab69dd2
imam76 <asepimam335@gmail.com>
2024-03-07 14:47:23 +0700

    fix gagal buka aplikasi

--- public/electron.js
+++ public/electron.js
@@ -1,43 +1,11 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-const { autoUpdater } = require("electron-updater");
+const { autoUpdater, AppUpdater } = require("electron-updater");
 const path = require('node:path')
 
 function sendStatusToWindow(text) {
   log.info(text);
   win.webContents.send(text); // send notification to UI about Update
 }
-
-autoUpdater.on('error', () => {
-  sendStatusToWindow('update_error');
-})
-
-autoUpdater.on('update-not-available', () => {
-  sendStatusToWindow('update_not_available');
-});
-
-autoUpdater.on('update-available', () => {
-  sendStatusToWindow('update_available');
-  dialog.showMessageBox({
-    type: 'info',
-    title: 'Update Tersedia',
-    message: 'Versi baru telah tersedia. Sedang mendownload ...',
-  });
-});
-
-autoUpdater.on('update-downloaded', () => {
-  sendStatusToWindow('update_downloaded');
-  dialog.showMessageBox({
-    type: 'info',
-    title: 'Update Siap',
-    message: 'Update telah didownload. Aplikasi akan ditutup dan diperbarui...',
-  }, () => {
-    autoUpdater.quitAndInstall();
-  });
-});
-
-app.on('ready', () => {
-  autoUpdater.checkForUpdatesAndNotify();
-});
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -53,21 +21,61 @@ function createWindow() {
   })
 
   win.webContents.openDevTools()
-  win.loadURL('index.html')
+  // win.loadURL('index.html')
+  win.loadURL('http://localhost:3000')
 }
 
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  app.on('ready', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+
+  autoUpdater.on('update-not-available', () => {
+    sendStatusToWindow('update_not_available');
+  });
+
+  autoUpdater.on('update-available', () => {
+    sendStatusToWindow('update_available');
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Update Tersedia',
+      message: 'Versi baru telah tersedia. Sedang mendownload ...',
+    });
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    sendStatusToWindow('update_downloaded');
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Update Siap',
+      message: 'Update telah didownload. Aplikasi akan ditutup dan diperbarui...',
+    }, () => {
+      autoUpdater.quitAndInstall();
+    });
+  });
+
+  autoUpdater.on('error', () => {
+    sendStatusToWindow('update_error');
+  })
+
+})
+
+// Ini akan mengembalikan versi aplikasi dari package.json
+ipcMain.handle('get-app-version', async () => {
+  return app.getVersion();
+});
+
+//Global exception handler
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
-
-app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-ipcMain.handle('get-app-version', async () => {
-  return app.getVersion(); // Ini akan mengembalikan versi aplikasi dari package.json
-});
